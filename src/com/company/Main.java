e1dbc78666ae6a7a5f8e632ac218194b06ea7721
rishiraam <52689843+rishiraam@users.noreply.github.com>
2022-02-09 13:39:07 +0530

    Update Main.java
    changes madi in github

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    //added comment to check pull
         Game g=new Game();
         g.test1();
         g.test2();
